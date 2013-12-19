6356d2600ab5c07600602d7349fa340f1e67d3cc
sky.liao <skyliao99@gmail.com>
2013-12-19 13:55:06 +0800

    test egit

--- Helloworld/src/com/sky/helloworld/MainActivity.java
+++ Helloworld/src/com/sky/helloworld/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         //sky.liao add for test git.
+        //git from eclipse.
     }
 
     @Override
